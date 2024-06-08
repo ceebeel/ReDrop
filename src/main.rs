9c39f5e3b53f2a48ab530ec6e751f6cf5cd5a631
CeeBeeL <c.blervacques@free.fr>
2024-06-08 21:05:52 +0200

    scan and show presets

--- src/main.rs
+++ src/main.rs
@@ -1,85 +1,120 @@
 use eframe::egui;
-use std::path::Path;
-use std::path::PathBuf;
+use std::collections::BTreeMap;
+use std::fs;
+use std::path::{Path, PathBuf};
+
 pub type FrameRate = u32;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
-    let options = eframe::NativeOptions {
-        // renderer: eframe::Renderer::Glow,
-        // viewport: egui::ViewportBuilder::default()
-        // .with_inner_size([800.0, 600.0]),
-        // .with_min_inner_size([800.0, 600.0]),
-        ..Default::default()
-    };
+    env_logger::init();
+    let options = eframe::NativeOptions::default();
     eframe::run_native(
         "ReDrop",
         options,
-        Box::new(|_cc| Box::<ReDropApp>::default()),
+        Box::new(|cc| {
+            egui_extras::install_image_loaders(&cc.egui_ctx);
+            Box::new(ReDropApp::new())
+        }),
     )?;
+
     Ok(())
 }
+
+#[allow(dead_code)]
 #[derive(Debug)]
 struct Preset {
+    index: usize,
     name: String,
     path: PathBuf,
-    img: String,
-    // category: String,
-    // subcategory: String,
+    img: Option<PathBuf>,
 }
 
+#[derive(Debug)]
+enum Node {
+    PresetId(usize),
+    Directory(BTreeMap<String, Node>),
+}
+
+#[derive(Default)]
 struct ReDropApp {
     presets: Vec<Preset>,
-}
-
-impl Default for ReDropApp {
-    fn default() -> Self {
-        let presets = ReDropApp::list_presets(Path::new("./presets"));
-        print!("Presets: {:#?}", presets);
-
-        ReDropApp { presets }
-    }
+    presets_tree: BTreeMap<String, Node>,
 }
 
 impl ReDropApp {
-    fn list_presets(path: &Path) -> Vec<Preset> {
-        let mut presets = Vec::new();
-        let path = Path::new(path);
-        for entry in std::fs::read_dir(path).unwrap() {
+    fn new() -> Self {
+        let mut slf = Self::default();
+        slf.update_presets_tree();
+        slf
+    }
+
+    fn scan_presets(&mut self, path: &Path) -> BTreeMap<String, Node> {
+        let mut dir = BTreeMap::new();
+        for entry in fs::read_dir(path).unwrap() {
             let entry = entry.unwrap();
             let path = entry.path();
-
-            if path.is_file() && path.extension().unwrap() == "milk" {
-                presets.push(Preset {
-                    name: path.file_stem().unwrap().to_str().unwrap().to_string(),
-                    // path: path.to_str().unwrap().to_string(),
-                    path,
-                    img: "".to_string(),
-                });
+            if path.is_dir() {
+                let mut sub_dir = BTreeMap::new();
+                sub_dir.extend(self.scan_presets(&path));
+                dir.insert(
+                    path.file_name().unwrap().to_string_lossy().into_owned(),
+                    Node::Directory(sub_dir),
+                );
+            } else if let Some(extension) = path.extension() {
+                if extension == "milk" {
+                    let name = path.file_stem().unwrap().to_string_lossy().into_owned();
+                    let img = path.with_extension("jpg");
+                    let preset_id = self.presets.len();
+                    let preset = Preset {
+                        index: preset_id,
+                        name: name.clone(),
+                        path: path.clone(),
+                        img: if img.exists() { Some(img) } else { None },
+                    };
+                    self.presets.push(preset);
+                    dir.insert(name, Node::PresetId(preset_id));
+                }
             }
         }
-        presets
+        dir
+    }
+
+    fn update_presets_tree(&mut self) {
+        self.presets.clear();
+        // TODO: Take presets path from config
+        self.presets_tree = self.scan_presets(Path::new("Presets"));
+    }
+
+    fn render_presets_tree(&self, ui: &mut egui::Ui, dir: &BTreeMap<String, Node>) {
+        for (name, node) in dir {
+            match node {
+                Node::PresetId(preset_id) => {
+                    let preset = &self.presets[*preset_id];
+                    if let Some(img_path) = &preset.img {
+                        if ui
+                            .image("file://".to_owned() + img_path.to_str().unwrap())
+                            .clicked()
+                        {
+                            println!("Preset:{:#?}", preset);
+                        }
+                    } else if ui.button(&preset.name).clicked() {
+                        println!("Preset:{:#?}", preset);
+                    }
+                }
+                Node::Directory(sub_dir) => {
+                    egui::CollapsingHeader::new(name).show(ui, |ui| {
+                        self.render_presets_tree(ui, sub_dir);
+                    });
+                }
+            }
+        }
     }
 }
 
 impl eframe::App for ReDropApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.collapsing("Category 1", |ui| {
-                ui.collapsing("SubCategory 1", |ui| {
-                    ui.label("Preset Name");
-                    ui.label("Preset Name");
-                    ui.label("Preset Name");
-                    // ...
-                });
-                ui.collapsing("SubCategory 2", |ui| {
-                    ui.label("Preset Name");
-                    ui.label("Preset Name");
-                    // ...
-                });
-                // ...
-            });
-            // ...
+            self.render_presets_tree(ui, &self.presets_tree);
         });
     }
 }
