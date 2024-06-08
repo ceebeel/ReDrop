68beceb72c5845d759a3155a1582e6e99ea89b83
CeeBeeL <c.blervacques@free.fr>
2024-06-09 00:24:55 +0200

    refact

--- src/main.rs
+++ src/main.rs
@@ -20,10 +20,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-#[allow(dead_code)]
 #[derive(Debug)]
 struct Preset {
-    // index: usize, //TODO: Remove iy unused
+    id: usize,
     name: String,
     path: PathBuf,
     img: Option<PathBuf>,
@@ -32,7 +31,7 @@ struct Preset {
 #[derive(Debug)]
 enum Node {
     PresetId(usize),
-    Directory(BTreeMap<String, Node>),
+    InnerNode(BTreeMap<String, Node>),
 }
 
 #[derive(Default)]
@@ -49,65 +48,73 @@ impl ReDropApp {
     }
 
     fn scan_presets(&mut self, path: &Path) -> BTreeMap<String, Node> {
-        let mut dir = BTreeMap::new();
+        let mut node = BTreeMap::new();
         for entry in fs::read_dir(path).unwrap() {
             let entry = entry.unwrap();
             let path = entry.path();
             if path.is_dir() {
-                let mut sub_dir = BTreeMap::new();
-                sub_dir.extend(self.scan_presets(&path));
-                dir.insert(
+                let mut inner_node = BTreeMap::new();
+                inner_node.extend(self.scan_presets(&path));
+                node.insert(
                     path.file_name().unwrap().to_string_lossy().into_owned(),
-                    Node::Directory(sub_dir),
+                    Node::InnerNode(inner_node),
                 );
-            } else if let Some(extension) = path.extension() {
-                if extension == "milk" {
-                    let name = path.file_stem().unwrap().to_string_lossy().into_owned();
-                    let img = path.with_extension("jpg");
-                    let preset_id = self.presets.len();
-                    let preset = Preset {
-                        // index: preset_id, //TODO: Remove iy unused
-                        name: name.clone(),
-                        path: path.clone(),
-                        img: if img.exists() { Some(img) } else { None },
-                    };
-                    self.presets.push(preset);
-                    dir.insert(name, Node::PresetId(preset_id));
-                }
+            } else if path.extension().unwrap() == "milk" {
+                let name = path.file_stem().unwrap().to_string_lossy().into_owned();
+                let img = path.with_extension("jpg");
+                let preset_id = self.presets.len();
+                let preset = Preset {
+                    id: preset_id,
+                    name: name.clone(),
+                    path: path.clone(),
+                    img: if img.exists() { Some(img) } else { None },
+                };
+                node.insert(name, Node::PresetId(preset_id));
+                self.presets.push(preset);
             }
         }
-        dir
+        node
     }
 
     fn update_presets_tree(&mut self) {
         self.presets.clear();
+        self.presets_tree.clear();
         // TODO: Take presets path from config
         self.presets_tree = self.scan_presets(Path::new("Presets"));
     }
 
-    fn render_presets_tree(&self, ui: &mut egui::Ui, dir: &BTreeMap<String, Node>) {
-        for (name, node) in dir {
+    fn send_load_preset_request(&self, preset_id: usize) {
+        println!("Load preset: {:#?}", preset_id);
+    }
+
+    // UI
+    fn show_preset(&self, ui: &mut egui::Ui, preset_id: &usize) {
+        let preset = &self.presets[*preset_id];
+        if let Some(img_path) = &preset.img {
+            let file_path = "file://".to_owned() + img_path.to_str().unwrap();
+            // TODO: Add image button into a Grid (Responsive ?)
+            if ui
+                .add(egui::ImageButton::new(
+                    egui::Image::new(file_path).fit_to_exact_size(egui::Vec2::new(64.0, 64.0)),
+                ))
+                .clicked()
+            {
+                self.send_load_preset_request(preset.id)
+            }
+        } else if ui.button(&preset.name).clicked() {
+            self.send_load_preset_request(preset.id)
+        }
+    }
+
+    fn show_presets_tree(&self, ui: &mut egui::Ui, node: &BTreeMap<String, Node>) {
+        for (name, node) in node {
             match node {
                 Node::PresetId(preset_id) => {
-                    let preset = &self.presets[*preset_id];
-                    if let Some(img_path) = &preset.img {
-                        let file_path = "file://".to_owned() + img_path.to_str().unwrap();
-                        // TODO: Add image button into a Grid (Responsive ?)
-                        if ui
-                            .add(egui::ImageButton::new(
-                                egui::Image::new(file_path).max_size(egui::Vec2::new(64.0, 64.0)),
-                            ))
-                            .clicked()
-                        {
-                            println!("Preset:{:#?}", preset);
-                        }
-                    } else if ui.button(&preset.name).clicked() {
-                        println!("Preset:{:#?}", preset);
-                    }
+                    self.show_preset(ui, preset_id);
                 }
-                Node::Directory(sub_dir) => {
+                Node::InnerNode(inner_node) => {
                     egui::CollapsingHeader::new(name).show(ui, |ui| {
-                        self.render_presets_tree(ui, sub_dir);
+                        self.show_presets_tree(ui, inner_node);
                     });
                 }
             }
@@ -119,7 +126,7 @@ impl eframe::App for ReDropApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         // TODO: Add Scroll
         egui::CentralPanel::default().show(ctx, |ui| {
-            self.render_presets_tree(ui, &self.presets_tree);
+            self.show_presets_tree(ui, &self.presets_tree);
         });
     }
 }
